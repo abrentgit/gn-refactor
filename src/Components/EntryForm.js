d31e7ee27e38584d28724aeb743da2863aa03247
abrentgit <abprojects.ca@gmail.com>
2019-10-07 20:57:36 -0400

    initial

--- src/Components/EntryForm.js
+++ src/Components/EntryForm.js
@@ -3,9 +3,9 @@ import './EntryForm.css';
 import { connect } from 'react-redux';
 import { Field, reduxForm, focus } from 'redux-form';
 import { postEntry } from '../actions/entryActions';
-import { required, nonEmpty } from '../validators';
 import Input from '../components/input';
 import { withRouter } from 'react-router-dom';
+import { required, nonEmpty } from '../validators.js';
 import './EntryForm.css';
 
 export class EntryForm extends React.Component {
@@ -14,52 +14,63 @@ export class EntryForm extends React.Component {
     console.log('entry submitted');
     this.props.dispatch(postEntry(values));
   }
+
   render() {
-    return (
-      <div>
-        <div id="container">
-          <p className="journal-question">
-            <i>What are you grateful for today?</i>
-          </p>
-          <span className="input message">
-            <textarea className="input__field" id="input-5" defaultValue={''} />
-            <label htmlFor="input-5" className="input__label">
-              <span className="input__label-content"></span>
-            </label>
-          </span>
-          <button id="send-button" type="button">
-            Send
-          </button>
+    let error;
+    if (this.props.error) {
+      error = (
+        <div className="form-error" aria-live="polite">
+          {this.props.error}
         </div>
-      </div>
-    );
-  }
-}
-/* <form
-        id="form"
+      );
+    }
+    return (
+      <form
         className="entry-form"
         onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
       >
-        <label htmlFor="entry" className="entry">
-          Create Journal Entry
-        </label>
+        {error}
+        <img className="logo" src="../ostrich.png" alt="ostrich" />
+        <p className="journal-question">
+          <i>What are you grateful for today?</i>
+        </p>
+        <label htmlFor="entry"></label>
         <Field
           component={Input}
-          id="message"
           type="text"
+          name="text"
+          id="entry-text"
           validate={[required, nonEmpty]}
-          placeholder="What are you grateful for?"
-          defaultValue={''}
-          aria-required="true"
         />
         <button
-          id="submit"
-          type="submit"
+          id="send-button"
           disabled={this.props.pristine || this.props.submitting}
         >
-          Submit
+          Send
         </button>
-      </form> */
+      </form>
+    );
+  }
+}
+
+//   render() {
+//     return (
+//       <div>
+//         <img className="logo" src="../ostrich.png" alt="ostrich" />
+//         <div id="container">
+//           <p className="journal-question">
+//             <i>What are you grateful for today?</i>
+//           </p>
+//           <div className="input message">
+//             <label htmlFor="input-5" className="input__label">
+//               <Field component={Input} defaultValue={''} />
+//             </label>
+//           </div>
+//           <button id="send-button" type="button">
+//             Send
+//           </button>
+//         </div>
+//       </div>
 //     );
 //   }
 // }
@@ -73,55 +84,3 @@ export default reduxForm({
   onSubmitFail: (errors, dispatch) =>
     dispatch(focus('contact', Object.keys(errors)[0]))
 })(withRouter(connect(mapStateToProps)(EntryForm)));
-
-// import React from 'react';
-// import './EntryForm.css';
-// import { Field, reduxForm, focus } from 'redux-form';
-// // import { postEntry } from '../actions/entryActions';
-
-// // need to create input
-// // import Input from './input';
-// // need to create actions
-// // import { login } from '../actions/auth';
-
-// export class EntryForm extends React.Component {
-//   onSubmit(values) {
-//     return this.props.dispatch(postEntry(values.title, values.content));
-//   }
-
-//   render() {
-//     let error;
-//     if (this.props.error) {
-//       error = (
-//         <div className="form-error" aria-live="polite">
-//           {this.props.error}
-//         </div>
-//       );
-//     }
-//     return (
-//       <form
-//         className="entry-form"
-//         onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
-//       >
-//         {error}
-//         <label htmlFor="title-field">An Awesome Entry Title</label>
-//         <Field
-//           type="text"
-//           name="title"
-//           id="title"
-//           validate={[required, nonEmpty]}
-//         />
-//         <label htmlFor="entry-field">"What are you grateful for?"</label>
-//         <Field
-//           type="text"
-//           name="entry"
-//           id="entry"
-//           validate={[required, nonEmpty]}
-//         />
-//         <button disabled={this.props.pristine || this.props.submitting}>
-//           Submit
-//         </button>
-//       </form>
-//     );
-//   }
-// }
--- src/Components/entry-input.js
+++ src/Components/entry-input.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import './EntryForm.css';
 
-export default class Input extends React.Component {
+export default class EntryInput extends React.Component {
   componentDidUpdate(prevProps) {
     if (!prevProps.meta.active && this.props.meta.active) {
       this.input.focus();
